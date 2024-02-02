fb59fe47f48ddd6d27a9bfaff4f872e01c4dc45f
clevertonandrade <clevertonandrade0@gmail.com>
2024-02-02 00:18:33 -0300

    remove projects.json

---
+++ scripts/projects.js
@@ -0,0 +1,32 @@
+const projects = [
+  {
+    'name': 'Galeria LXL',
+    'description': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat tempore quas rem numquam recusandae vitae aperiam sunt at harum temporibus soluta corporis, perferendis tenetur neque magnam assumenda ab dolor quasi!',
+    'image': './assets/galerialxl.png',
+    'link': 'https://www.instagram.com/galerialxl/'
+  },
+  {
+    'name': 'Starifmg',
+    'description': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat tempore quas rem numquam recusandae vitae aperiam sunt at harum temporibus soluta corporis, perferendis tenetur neque magnam assumenda ab dolor quasi!',
+    'image': './assets/starifmg.png',
+    'link': 'https://www.instagram.com/starifmg/'
+  },
+  {
+    'name': 'Code System',
+    'description': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat tempore quas rem numquam recusandae vitae aperiam sunt at harum temporibus soluta corporis, perferendis tenetur neque magnam assumenda ab dolor quasi!',
+    'image': './assets/codesystem.png',
+    'link': 'https://www.instagram.com/codesystem.ifmg/'
+  },
+  {
+    'name': 'E-books desenvolvidos',
+    'description': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat tempore quas rem numquam recusandae vitae aperiam sunt at harum temporibus soluta corporis, perferendis tenetur neque magnam assumenda ab dolor quasi!',
+    'image': './assets/ebooks.png',
+    'link': 'https://www.instagram.com/gustavolxl/'
+  },
+  {
+    'name': 'Sinapse',
+    'description': 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat tempore quas rem numquam recusandae vitae aperiam sunt at harum temporibus soluta corporis, perferendis tenetur neque magnam assumenda ab dolor quasi!',
+    'image': './assets/sinapse.png',
+    'link': 'https://www.instagram.com/galerialxl/'
+  }
+] 
--- scripts/script.js
+++ scripts/script.js
@@ -15,42 +15,38 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const cardsContainer = document.getElementById('cards');
 
-  fetch('../projects.json').then((response) => {
-    return response.json();
-  }).then((data) => {
-    data.forEach((project, index) => {
-      const card = document.createElement("div");
-      card.setAttribute('class', 'card');
+  projects.forEach((project, index) => {
+    const card = document.createElement("div");
+    card.setAttribute('class', 'card');
 
-      const a = document.createElement("a");
-      a.setAttribute('href', project.link);
+    const a = document.createElement("a");
+    a.setAttribute('href', project.link);
 
-      const img = document.createElement("img");
-      img.setAttribute('class', 'preview__image');
-      img.setAttribute('src', project.image);
-      img.setAttribute('alt', project.name);
+    const img = document.createElement("img");
+    img.setAttribute('class', 'preview__image');
+    img.setAttribute('src', project.image);
+    img.setAttribute('alt', project.name);
 
-      const section = document.createElement("section");
-      section.setAttribute('class', 'project__details');
+    const section = document.createElement("section");
+    section.setAttribute('class', 'project__details');
 
-      const projectName = document.createElement("p");
-      projectName.setAttribute('class', 'project__name');
-      projectName.innerText = project.name;
+    const projectName = document.createElement("p");
+    projectName.setAttribute('class', 'project__name');
+    projectName.innerText = project.name;
 
-      const projectDescription = document.createElement("span");
-      projectDescription.setAttribute('class', 'project__description');
-      projectDescription.innerText = project.description;
+    const projectDescription = document.createElement("span");
+    projectDescription.setAttribute('class', 'project__description');
+    projectDescription.innerText = project.description;
 
-      cardsContainer.appendChild(a);
-      a.appendChild(card);
-      card.appendChild(img);
-      card.appendChild(section);
-      section.appendChild(projectName);
-      section.appendChild(projectDescription);
+    cardsContainer.appendChild(a);
+    a.appendChild(card);
+    card.appendChild(img);
+    card.appendChild(section);
+    section.appendChild(projectName);
+    section.appendChild(projectDescription);
 
-      card.style.animationDelay = `${index * 0.5}s`;
-      card.classList.add('animate-up');
-    });
+    card.style.animationDelay = `${index * 0.5}s`;
+    card.classList.add('animate-up');
   });
 
   const element = document.querySelector('.animate-up');
