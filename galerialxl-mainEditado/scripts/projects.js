2249008d2e65388e20042d10ada422ed4b614cf2
Gustavo Lopes <108312163+gustavolxl@users.noreply.github.com>
2024-02-07 15:04:52 -0300

    Add files via upload

---
+++ galerialxl-mainEditado/scripts/projects.js
@@ -0,0 +1,32 @@
+const projects = [
+  {
+    'name': 'Galeria LXL',
+    'description': 'Durante minhas atividades precisei desenvolver logos e artes para os projetos, e então isso se tornou um hobby, aqui você pode conferir alguns dos resultados',
+    'image': './assets/galerialxl.png',
+    'link': 'https://www.instagram.com/starifmg/'
+  },
+  {
+    'name': 'Starifmg',
+    'description': 'Starif é um programa educacional desenvolvido no IFMG - Campus Ibirité, que envolve os alunos em atividades formentadas pelo MCTI, NASA e Las Cumbes Observatory, promovendo a exploração e o estudo do universo.',
+    'image': './assets/starifmg.png',
+    'link': 'https://www.instagram.com/starifmg/'
+  },
+  {
+    'name': 'Code System',
+    'description': 'Trata-se de uma empresa incubada no Ambiente de Inovação - FLUI, cujo objetivo é fortalecer a presença online de empreendedores locais por meio da criação de websites e uma identidade visual robusta.',
+    'image': './assets/codesystem.jpg',
+    'link': 'https://www.instagram.com/codesystem.ifmg/'
+  },
+  {
+    'name': 'E-books desenvolvidos',
+    'description': 'Sob o apoio da FAPEMIG publiquei dois eBooks: o primeiro aborda a formalização do Microempreendedor Individual, enquanto o segundo explora a plataforma Thunkable para programação em blocos. Acessando essa página você pode baixar ambos conteúdos.',
+    'image': './assets/ebooks.png',
+    'link': 'https://www.instagram.com/gustavolxl/'
+  },
+  {
+    'name': 'Sinapse',
+    'description': 'Projeto desenvolvido e selecionado como finalista na VI Olimpíada de Inovação, visando a diminuição do desperdício de alimentos perecíveis em hortifrutis, sacolões, supermercados e entre os consumidores.',
+    'image': './assets/sinapse.png',
+    'link': 'https://www.instagram.com/galerialxl/'
+  }
+] 
